390d7b04db87a3584baf3fad715ad6378640f9fb
Dinh Long <nugyendinhlong51004@gmail.com>
2025-10-21 20:59:35 +0700

    Push_01

--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -1,6 +1,33 @@
-﻿namespace Give_AID.Models
+﻿using Give_AID.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace GiveAid.API.Models
 {
     public class User
     {
+        public int Id { get; set; }
+
+        [Required, MaxLength(120)]
+        public string FullName { get; set; }
+
+        [Required, EmailAddress, MaxLength(200)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [MaxLength(20)]
+        public string Role { get; set; } = "User";
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Donation> Donations { get; set; }
+        public ICollection<Invitation> SentInvitations { get; set; }
     }
 }
